cb674c18ccb2b0271505d04c830b5b1694f3eed6
Abhay Gupta <abhaygupta7531@gmail.com>
2026-05-22 11:50:34 +0530

    1 pre

---
+++ preprocessor.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define height 100
+#define number 3.14
+#define letter 'A'
+#define letter_sequence "Hello, World!"
+#define special_char '?'
+
+int main() {
+    printf("Height: %d\n", height);
+    printf("Number: %.2f\n", number);
+    printf("Letter: %c\n", letter);
+    printf("Letter Sequence: %s\n", letter_sequence);
+    printf("Special Character: %c\n", special_char);
+    
+    return 0;
+}
